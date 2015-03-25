8a071ec3b28735cb2f0c525d9c7d665d2deebb08
Torbjorn Lundberg <torbjorn.lundberg@softhouse.se>
2015-03-25 14:53:05 +0100

    Added an inital index.html file to serve

--- server/server.js
+++ server/server.js
@@ -2,5 +2,6 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
-app.use('/', express.static(path.join(__dirname, '../dist')));
+// You probably want to change 'dist/' to '../dist/' for production
+app.use('/', express.static(path.join(__dirname, 'dist/')));
 app.listen(9000);

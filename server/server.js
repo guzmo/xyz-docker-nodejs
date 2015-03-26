8836c3260692f5d8bb929e1676b169f7211d87fd
andreas <andreas.tornstrom.andersson@gmail.com>
2015-03-26 18:36:07 +0100

    Testing headers for auth

--- server/server.js
+++ server/server.js
@@ -4,6 +4,12 @@ var app = express();
 
 // You probably want to change 'dist/' to '../dist/' for production
 app.use('/', express.static(path.join(__dirname, 'dist/')));
+
+app.get('/kalle', function(req, res) {
+    console.log("ASD");
+    res.send(JSON.stringify(req.headers));
+});
+
 var server = app.listen(9000, function () {
     console.log('Server started: http://localhost:%s/', server.address().port);
 });

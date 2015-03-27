319c98e1e650c666b942efa96b0a525c251b585b
Emir <emir.mehic@softhouse.se>
2015-03-27 16:38:54 +0100

    app test

--- server/dist/app.js
+++ server/dist/app.js
@@ -2,11 +2,10 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
+  'ngRoute']).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+  $routeProvider.otherwise({redirectTo: '/'});
+}]).
+    controller("demoController", function(){
+        alert("alo");
+    });

59564b68ef71eb48dce84593701fc569a25d977e
Emir <emir.mehic@softhouse.se>
2015-03-27 16:58:38 +0100

    getting the api data

--- server/dist/view1/view1.js
+++ server/dist/view1/view1.js
@@ -9,6 +9,9 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
-
+.controller('View1Ctrl', [function($http, $scope) {
+    http.get("xyz.softhouse.se/api/buildqueue").success(function(response){
+        $scope.allData = response;
+        console.log(response);
+    });
 }]);
